4e0773467eb7081419aa87eea7d8958c204b04b0
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-10 13:39:57 +0100

    Put config constants into namespace

--- main/config.h
+++ main/config.h
@@ -1,4 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #include <cstdint>
+
+namespace CONFIG {
 
 constexpr char* WIFI_SSID = <Your WiFi SSID>;
 constexpr char* WIFI_PASSWORD = <Your WiFi password>;
@@ -6,3 +11,7 @@ constexpr char* WIFI_PASSWORD = <Your WiFi password>;
 constexpr char* THINGSBOARD_DEVICE_TOKEN = <Your ThingsBoard device token>;
 constexpr char* THINGSBOARD_SERVER = <Your ThingsBoard server url>;
 constexpr uint16_t THINGSBOARD_PORT = <Your ThingsBoard server port>;
+
+};  // namespace CONFIG
+
+#endif  // CONFIG_H
