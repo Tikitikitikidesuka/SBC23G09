1672c3fb764213f482c9cb0a273733005cf559f9
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-11 00:35:58 +0100

    Add RPC listening support

--- main/main.cpp
+++ main/main.cpp
@@ -11,36 +11,53 @@
 
 constexpr char TAG[] = "MAIN";
 
+constexpr const char RPC_START_CALLBACK_METHOD[] = "start";
+
 constexpr uint16_t MAX_MESSAGE_SIZE = 1024;
 
 Espressif_MQTT_Client mqtt_client;
 ThingsBoard tb(mqtt_client, MAX_MESSAGE_SIZE);
 
 bool tb_connect();
+RPC_Response start_callback(const RPC_Data& data);
+
+const std::array<RPC_Callback, 1U> RPC_CALLBACKS = {
+    RPC_Callback{RPC_START_CALLBACK_METHOD, start_callback},
+};
 
 extern "C" void app_main() {
     nvs_flash_init();
 
     sta_start();
-    sta_connect(CONFIG::WIFI_SSID, CONFIG::WIFI_PASSWORD, 5);
 
-    tb_connect();
+    bool subscribed = false;
 
     while (true) {
         ESP_LOGI(TAG, "Iteration");
 
-        if (!tb.connected()) {
-            ESP_LOGI(TAG, "Failed to connect to ThingsBoard server");
+        if (!sta_connected()) {
+            ESP_LOGI(TAG, "Reconnecting to WiFi");
+            if (sta_connect(CONFIG::WIFI_SSID, CONFIG::WIFI_PASSWORD, 5))
+                ESP_LOGI(TAG, "WiFi connection succeeded");
+        } else if (!tb.connected()) {
+            subscribed = false;
+            ESP_LOGI(TAG, "Reconnecting to ThingsBoard server");
             if (tb_connect())
                 ESP_LOGI(TAG, "Connection to ThingsBoard server succeeded");
+        } else if (!subscribed) {
+            ESP_LOGI(TAG, "Subscribing to RPC");
+            if (tb.RPC_Subscribe(RPC_CALLBACKS.cbegin(), RPC_CALLBACKS.cend())) {
+                ESP_LOGI(TAG, "RPC subscription succeeded");
+                subscribed = true;
+            }
         } else {
             ESP_LOGI(TAG, "Sending water level...");
             tb.sendTelemetryData("water_level", esp_random() % 101);
             tb.sendTelemetryData("temperature", esp_random() % 20 + 10);
-        }
 
-        if (!sta_connected())
-            sta_connect(CONFIG::WIFI_SSID, CONFIG::WIFI_PASSWORD, 5);
+            // Process messages
+            tb.loop();
+        }
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
@@ -50,6 +67,11 @@ bool tb_connect() {
     ESP_LOGI(TAG, "Connecting to: %s", CONFIG::THINGSBOARD_SERVER);
     return tb.connect(CONFIG::THINGSBOARD_SERVER, CONFIG::THINGSBOARD_DEVICE_TOKEN,
                       CONFIG::THINGSBOARD_PORT);
+}
+
+RPC_Response start_callback(const RPC_Data& data) {
+    ESP_LOGI(TAG, "Start!!!");
+    return RPC_Response("XD", 33);
 }
 
 /*
