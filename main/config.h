5020bf9b23eda8aaa68844621f8f638464bbd97a
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-10 13:31:31 +0100

    Add empty config header file

---
+++ main/config.h
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+constexpr char* WIFI_SSID = <Your WiFi SSID>;
+constexpr char* WIFI_PASSWORD = <Your WiFi password>;
+
+constexpr char* THINGSBOARD_DEVICE_TOKEN = <Your ThingsBoard device token>;
+constexpr char* THINGSBOARD_SERVER = <Your ThingsBoard server url>;
+constexpr uint16_t THINGSBOARD_PORT = <Your ThingsBoard server port>;
