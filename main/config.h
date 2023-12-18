ef94e83cfee3ef5764f7bd7d63206accaa6263a7
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-18 17:12:37 +0100

    Update config header

--- main/config.h
+++ main/config.h
@@ -5,9 +5,6 @@
 
 namespace CONFIG {
 
-constexpr char* WIFI_SSID = "SBC";
-constexpr char* WIFI_PASSWORD = "SBCwifi$";
-
 constexpr char* THINGSBOARD_DEVICE_TOKEN = "wl9ttk9kwixa5f2vppar";
 constexpr char* THINGSBOARD_SERVER = "demo.thingsboard.io";
 constexpr uint16_t THINGSBOARD_PORT = 1883;
